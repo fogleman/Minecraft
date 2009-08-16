2713471a59cdc699c77c0729cea9177d98422ac7
Tristam MacDonald <swiftcoder@gmail.com>
2009-08-16 16:07:50 -0400

    Add workaround to speed up label rendering.

--- simplui/shape.py
+++ simplui/shape.py
@@ -73,6 +73,38 @@ class Rectangle(object):
 			self.patch.update_vertex_list(self._vertices, x, y, w, h)
 
 class BasicLabel(pyglet.text.Label):
+	### workaround for pyglet issue 427
+	_cached_groups = {} 
+	def _init_groups(self, group): 
+		if not group: 
+			return 
+		if group not in self.__class__._cached_groups.keys(): 
+			top = pyglet.text.layout.TextLayoutGroup(group) 
+			bg = pyglet.graphics.OrderedGroup(0,top) 
+			fg = pyglet.text.layout.TextLayoutForegroundGroup(1,top) 
+			fg2 = pyglet.text.layout.TextLayoutForegroundDecorationGroup (2,top) 
+			self.__class__._cached_groups[group] = [top,bg,fg,fg2,0] 
+		groups = self.__class__._cached_groups[group] 
+		self.top_group= groups[0] 
+		self.background_group = groups[1] 
+		self.foreground_group = groups[2] 
+		self.foreground_decoration_group = groups[3] 
+		groups[4] += 1
+	
+	def delete(self): 
+		pyglet.text.Label.delete(self) 
+		group = self.top_group.parent 
+		if group is not None: 
+			groups = self.__class__._cached_groups[group] 
+			groups[4] -= 1 
+			if not groups[4]: 
+				del self.__class__._cached_groups[group] 
+			self.top_group = None 
+			self.background_self = None 
+			self.foreground_group = None 
+			self.foreground_decoration_group = None 
+	### end workaround
+	
 	def update_batch(self, batch, group):
 		self.delete()
 		
@@ -103,10 +135,12 @@ class EditableLabel(object):
 		self.caret.delete()
 		self.layout.delete()
 		
+		### workaround for pyglet issue 408
 		self.layout.batch = None
 		if self.layout._document:
 			self.layout._document.remove_handlers(self.layout)
 		self.layout._document = None
+		### end workaround
 		
 		self.layout = pyglet.text.layout.IncrementalTextLayout(self.document, self.layout.width, self.layout.height, multiline=False, batch=batch, group=group)
 		self.caret = pyglet.text.caret.Caret(self.layout)
