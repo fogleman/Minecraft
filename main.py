f5827045a0022a04cba25bda68dec73989d90b6e
Piotr Grabowski <avelanarius+github@gmail.com>
2013-03-31 18:22:36 +0200

    Tweaked textures to be more vibrant and simillar to original Minecraft
    I've tweaked textures to be more contrast and better-looking with fog.
    Sand texture is now more vibrant and distinguishable from far. Sky
    color and textures are more realistic and similar to original Minecraft.

--- main.py
+++ main.py
@@ -450,7 +450,7 @@ class Window(pyglet.window.Window):
 
 def setup_fog():
     glEnable(GL_FOG)
-    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.53, 0.81, 0.98, 1))
+    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.5, 0.69, 1.0, 1))
     glHint(GL_FOG_HINT, GL_DONT_CARE)
     glFogi(GL_FOG_MODE, GL_LINEAR)
     glFogf(GL_FOG_DENSITY, 0.35)
@@ -458,7 +458,7 @@ def setup_fog():
     glFogf(GL_FOG_END, 60.0)
 
 def setup():
-    glClearColor(0.53, 0.81, 0.98, 1)
+    glClearColor(0.5, 0.69, 1.0, 1)
     glEnable(GL_CULL_FACE)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
