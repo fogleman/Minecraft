d039007f128b1ce7499a642abd0a33a82e703c9e
Ronald Brad Murphy <solo.dev@gmail.com>
2013-04-03 11:07:22 -0400

    block checking, better dirt under grass
    not perfect, but it works.  some grass on grass overlap may happen.

--- main.py
+++ main.py
@@ -256,7 +256,9 @@ class Model(object):
                         if (x - 0) ** 2 + (z - 0) ** 2 < 5 ** 2:
                             continue
                         self.init_block((x, y, z), t)
-                        self.init_block((x - 1, y - 1, z), dirt_block)
+                        if t == grass_block:
+                            self.init_block((x - 1, y - 1, z), dirt_block)
+                            self.init_block((x - 2, y - 2, z), dirt_block)
 
                     #if WORLDTYPE == 5: # cover the mountains of stone with grass
                         #self.init_block((x + 1, y + 1, z + 1), grass_block)
