672e725a0e58937c5a52cbd69dde1fbfcf09eee6
Wojciech Zylinski <voitek@boskee.co.uk>
2013-04-03 23:36:34 +0100

    Enable texture transparency. Needs further improvements (drawing order) to make sure DEPTH_TEST is correct

--- main.py
+++ main.py
@@ -955,6 +955,7 @@ def setup():
     glEnable(GL_LIGHT1)
     glEnable(GL_LIGHT2)
     glEnable(GL_CULL_FACE)
+    glEnable(GL_BLEND)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
 
