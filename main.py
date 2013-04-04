89cfd86176887fdb73ec3e3c1785c462720e4dab
Wojciech Zylinski <voitek@boskee.co.uk>
2013-04-04 14:49:00 +0100

    Default configuration use "plains" terrain

--- main.py
+++ main.py
@@ -54,13 +54,14 @@ SAVE_FILENAME = os.path.join(game_dir, 'save.dat')
 config = ConfigParser()
 config_file = os.path.join(game_dir, 'game.cfg')
 if not os.path.lexists(config_file):
+    type, hill_height, max_trees = terrain_options[0]
     config.add_section('World')
-    config.set('World', 'type', '0')  # 0=grass,1=dirt,2=desert,3=islands,4=sand,5=stone,6=snow
-    config.set('World', 'hill_height', '6')  # height of the hills, increase for mountains :D
+    config.set('World', 'type', str(type))  # 0=plains,1=dirt,2=desert,3=islands,4=sand,5=stone,6=snow
+    config.set('World', 'hill_height', str(hill_height))  # height of the hills, increase for mountains :D
     config.set('World', 'flat', '0')  # dont make mountains,  make a flat world
     config.set('World', 'size', '160')
     config.set('World', 'show_fog', '1')
-    config.set('World', 'max_trees', '500')  # Was RND_FOREST
+    config.set('World', 'max_trees', str(max_trees))
 
     try:
         with open(config_file, 'wb') as handle:
