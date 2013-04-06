46a4ac15931fdd662972646272932183ea51aa92
Avelanarius <avelanarius+github@gmail.com>
2013-04-06 11:26:40 +0200

    Temporary solution to sound problems.

--- sounds.py
+++ sounds.py
@@ -1,8 +1,5 @@
 import pyglet.media
 from os import path
 
-pyglet.resource.path = [path.join('resources', 'sounds')]
-pyglet.resource.reindex()
-
-wood_break = pyglet.resource.media('wood_break.wav', streaming=False)
+wood_break = pyglet.resource.media('wood_break.wav', streaming=False) # temporary solution
 water_break = pyglet.resource.media('water_break.wav', streaming=False)
