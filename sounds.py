51fdd22f8ba33de30360fe586fde9d576ab249e1
tfaris <ta.faris@gmail.com>
2013-04-06 02:00:38 -0400

    Fix sound resource paths
    - Set pyglet resource path and reindex the files so that it can
      find and load sound files.

--- sounds.py
+++ sounds.py
@@ -1,5 +1,8 @@
 import pyglet.media
 from os import path
 
-wood_break = pyglet.resource.media(path.join('resources', 'sounds', 'wood_break.wav'), streaming=False)
-water_break = pyglet.resource.media(path.join('resources', 'sounds', 'water_break.wav'), streaming=False)
+pyglet.resource.path = [path.join('resources', 'sounds')]
+pyglet.resource.reindex()
+
+wood_break = pyglet.resource.media('wood_break.wav', streaming=False)
+water_break = pyglet.resource.media('water_break.wav', streaming=False)
