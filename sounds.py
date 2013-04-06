51e6a1eae40160e68d00f049d8b204c90112c229
Wojciech Zylinski <voitek@boskee.co.uk>
2013-04-06 02:12:41 +0100

    Code reorganization. Moved *.wav to sounds/

--- sounds.py
+++ sounds.py
@@ -1,4 +1,4 @@
 import pyglet.media
 
-wood_break = pyglet.resource.media('wood_break.wav', streaming=False)
-water_break = pyglet.resource.media('water_break.wav', streaming=False)
+wood_break = pyglet.resource.media('sounds/wood_break.wav', streaming=False)
+water_break = pyglet.resource.media('sounds/water_break.wav', streaming=False)
