71ba163bf541fe93d15abacd1bdfd0b8a0891755
Avelanarius <avelanarius+github@gmail.com>
2013-04-07 13:08:30 +0200

    Fix positional sound.

--- sounds.py
+++ sounds.py
@@ -15,12 +15,13 @@ melon_break = pyglet.resource.media("melon_break.wav", streaming=False)
 sand_break = pyglet.resource.media("sand_break.wav", streaming=False)
 
 def play_sound(sound, player=None, position=None):
-    sound_player = pyglet.media.ManagedSoundPlayer()
-    pyglet.media.listener.volume = globals.EFFECT_VOLUME
-    if position:
-        sound_player.position = position
-    if sound_player:
-        pyglet.media.listener.position = player.position
-    sound_player.queue(sound)
-    sound_player.play()
-    return sound_player
+	pyglet.options['audio'] = ('openal', 'directsound', 'alsa', 'silent')
+	sound_player = pyglet.media.ManagedSoundPlayer()
+	pyglet.media.listener.volume = globals.EFFECT_VOLUME
+	if position:
+		sound_player.position = position
+	if sound_player:
+		pyglet.media.listener.position = player.position
+	sound_player.queue(sound)
+	sound_player.play()
+	return sound_player
