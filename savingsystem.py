364d2a7467751b0dfad7d598ad0a9b0bbeb6ce2b
Nebual <ben1120@gmail.com>
2013-04-07 22:32:00 -0700

    Saving: Added new save system: Flatfile_Struct Uses struct.pack to store blockdata and sectors. Currently only stores blockid and position, when blocks have more attributes they'll need to be added to the save format. savingsystem.py: Changed tabs to spaces, as per the rest of the project main.py: Changed launch option -nocompression to -save-mode

--- savingsystem.py
+++ savingsystem.py
@@ -2,43 +2,100 @@ import os
 import cPickle as pickle
 import zlib
 import cStringIO as StringIO
+import struct
+import math
 
 # Modules from this project
 from model import *
 from player import *
+import globals
 
 # Save types
 CLASSIC_SAVE_TYPE = 0
 COMPRESSED_SAVE_TYPE = 1
+FLATFILE_STRUCT_SAVE_TYPE = 2
+SAVE_TYPES = [CLASSIC_SAVE_TYPE, COMPRESSED_SAVE_TYPE, FLATFILE_STRUCT_SAVE_TYPE] # last is always the newest
 
-SAVE_TYPES = [CLASSIC_SAVE_TYPE, COMPRESSED_SAVE_TYPE] # last is always the newest
+structvec = struct.Struct("hhh")
+structushort = struct.Struct("H")
+structuchar2 = struct.Struct("BB")
+structvecBB = struct.Struct("hhhBB")
 
-def save_world(window, game_dir, world=None, save_type=COMPRESSED_SAVE_TYPE):
-	if world is None: world = "world"
-	if not os.path.exists(os.path.join(game_dir, world)):
-		os.makedirs(os.path.join(game_dir, world))
-	#window.model.items(), window.model.sectors
-	#Non block related data
-	save = (3,window.player, window.time_of_day)
-	pickle.dump(save, open(os.path.join(game_dir, world, "save.pkl"), "wb"))
+def save_world(window, game_dir, world=None):
+    if world is None: world = "world"
+    if not os.path.exists(os.path.join(game_dir, world)):
+        os.makedirs(os.path.join(game_dir, world))
 
-	blocks_save = (3,window.model.items(), window.model.sectors)
-	pickle.dump(blocks_save, open(os.path.join(game_dir, world, "blocks.pkl"), "wb"))
+    #Non block related data
+    save = (3,window.player, window.time_of_day)
+    pickle.dump(save, open(os.path.join(game_dir, world, "save.pkl"), "wb"))
+
+    #blocks and sectors (window.model and window.model.sectors)
+    if globals.LAUNCH_OPTIONS.save_mode == FLATFILE_STRUCT_SAVE_TYPE:
+        blocks = window.model
+        with open(os.path.join(game_dir, world, "blocks.dat"), "wb", 1024*1024) as f:
+            f.write(struct.pack("Q",len(blocks)))
+            for blockpos in blocks:
+                f.write(structvec.pack(*blockpos) + structuchar2.pack(blocks[blockpos].id_main, blocks[blockpos].id_sub))
+        sectors = window.model.sectors
+        with open(os.path.join(game_dir, world, "sectors.dat"), "wb", 1024*1024) as f:
+            f.write(struct.pack("Q",len(sectors)))
+            for secpos in sectors:
+                f.write(structvec.pack(*secpos) + structushort.pack(len(sectors[secpos])))
+                for blockpos in sectors[secpos]:
+                    f.write(structvec.pack(*blockpos))
+    elif globals.LAUNCH_OPTIONS.save_mode == COMPRESSED_SAVE_TYPE:
+        worldsave = (window.model.items(), window.model.sectors)
+        save_string = zlib.compress(pickle.dumps(worldsave), 9)
+        file = open(os.path.join(game_dir, world, "blocks.pkl"), "wb")
+        file.write(struct.pack("B", 1)) #Save Version
+        file.write(save_string)
+        file.close()
+    elif globals.LAUNCH_OPTIONS.save_mode == CLASSIC_SAVE_TYPE:
+        worldsave = (window.model.items(), window.model.sectors)
+        save_string = pickle.dumps(worldsave)
+        file = open(os.path.join(game_dir, world, "blocks.pkl"), "wb")
+        file.write(struct.pack("B", 0)) #Save Version
+        file.write(save_string)
+        file.close()
 
 def world_exists(game_dir, world=None):
-	if world is None: world = "world"
-	return os.path.lexists(os.path.join(game_dir, world))
+    if world is None: world = "world"
+    return os.path.lexists(os.path.join(game_dir, world))
 
 def open_world(gamecontroller, game_dir, world=None):
-	if world is None: world = "world"
-	loaded_world = pickle.load(open(os.path.join(game_dir, world, "blocks.pkl"), "rb"))
+    if world is None: world = "world"
+    gamecontroller.model = Model(initialize=False)
 
-	gamecontroller.model = Model(initialize=False)
-	for item in loaded_world[0]:
-		gamecontroller.model[item[0]] = item[1]
-	gamecontroller.model.sectors = loaded_world[1]
-
-	loaded_save = pickle.load(open(os.path.join(game_dir, world, "save.pkl"), "rb"))
-	if loaded_save[0] == 3: #Version 3
-		if isinstance(loaded_save[1], Player): gamecontroller.player = loaded_save[1]
-		if isinstance(loaded_save[2], float):  gamecontroller.time_of_day = loaded_save[2]
+    #Non block related data
+    loaded_save = pickle.load(open(os.path.join(game_dir, world, "save.pkl"), "rb"))
+    if loaded_save[0] == 3: #Version 3
+        if isinstance(loaded_save[1], Player): gamecontroller.player = loaded_save[1]
+        if isinstance(loaded_save[2], float): gamecontroller.time_of_day = loaded_save[2]
+    #blocks and sectors (window.model and window.model.sectors)
+    if globals.LAUNCH_OPTIONS.save_mode == FLATFILE_STRUCT_SAVE_TYPE:
+        sectors = gamecontroller.model.sectors
+        with open(os.path.join(game_dir, world, "sectors.dat"), "rb") as f:
+            for i in xrange(struct.unpack("Q",f.read(8))[0]):
+                sector = sectors[structvec.unpack(f.read(6))]
+                for i2 in xrange(structushort.unpack(f.read(2))[0]):
+                    sector.append(structvec.unpack(f.read(6)))
+        blocks = gamecontroller.model
+        with open(os.path.join(game_dir, world, "blocks.dat"), "rb") as f:
+            for i in xrange(struct.unpack("Q",f.read(8))[0]):
+                bx, by, bz, blockid, dataid = structvecBB.unpack(f.read(8))
+                if dataid is not 0: blockid += (blockid / 10**math.ceil(math.log10(dataid)))
+                blocks[(bx,by,bz)] = BLOCKS_DIR[blockid]
+    else:
+        file = open(os.path.join(game_dir, world, "blocks.pkl"), "rb")
+        fileversion = struct.unpack("B",file.read(1))[0]
+        if fileversion == COMPRESSED_SAVE_TYPE:
+            loaded_world = pickle.load(StringIO.StringIO(zlib.decompress(file.read())))
+            for item in loaded_world[0]:
+                gamecontroller.model[item[0]] = item[1]
+            gamecontroller.model.sectors = loaded_world[1]
+        elif fileversion == CLASSIC_SAVE_TYPE:
+            loaded_world = pickle.load(file)
+            for item in loaded_world[0]:
+                gamecontroller.model[item[0]] = item[1]
+            gamecontroller.model.sectors = loaded_world[1]
