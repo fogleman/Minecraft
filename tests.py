328f4eddd785fed493d44c3684068d4f81342c52
Avelanarius <avelanarius+github@gmail.com>
2013-04-12 15:52:53 +0200

    Added basic unit tests (for inventory)

---
+++ tests.py
@@ -0,0 +1,66 @@
+import unittest
+import random
+from inventory import Inventory
+from items import ItemStack
+import globals
+
+class InventoryTests(unittest.TestCase):
+
+    def test_init(self):
+        for size in [0, 9, random.randint(3, 100)]:
+            inv = Inventory(slot_count=size)
+            self.assertEqual(inv.slot_count, size)
+            self.assertEqual(inv.slots, [None] * size)
+
+    def test_find_empty_slot(self):
+        for size in [0, 9, random.randint(3, 100)]:
+            inv = Inventory(slot_count=size).find_empty_slot()
+            self.assertEqual(inv, 0 if size > 0 else -1)
+
+    def test_add_item_1(self):
+        for size in [0, 9, random.randint(3, 100)]:
+            inv = Inventory(slot_count=size)
+            item = random.choice(globals.ITEMS_DIR.keys())
+            block = random.choice(globals.BLOCKS_DIR.keys())
+            result = inv.add_item(item)
+            result2 = inv.add_item(block)
+            if size == 0:
+                self.assertFalse(result)
+                self.assertFalse(result2)
+            else:
+                self.assertTrue(result)
+                self.assertTrue(result2) 
+                foundItem = False
+                foundBlock = False
+                for slot in inv.slots:
+                    if slot:
+                        if slot.type == item and slot.amount == 1:
+                            foundItem = True
+                        elif slot.type == block and slot.amount == 1:
+                            foundBlock = True
+                self.assertTrue(foundItem)
+                self.assertTrue(foundBlock)
+            self.assertEqual(result, result2)
+
+    def test_add_item_2(self):
+        inv = Inventory(slot_count=20)
+        block = random.choice(globals.BLOCKS_DIR.keys())
+        max_items = globals.BLOCKS_DIR[block].max_stack_size * 20
+        for i in xrange(0, max_items):
+            self.assertTrue(inv.add_item(block))
+        item = random.choice(globals.ITEMS_DIR.keys())
+        inv2 = Inventory(slot_count=20)
+        max_items2 = globals.ITEMS_DIR[item].max_stack_size * 20
+        for i in xrange(0, max_items2):
+            self.assertTrue(inv2.add_item(item))
+        self.assertNotIn(None, inv.slots)
+        self.assertNotIn(None, inv2.slots)
+        for slot in inv.slots:
+            self.assertEqual(slot.type, block)
+            self.assertEqual(slot.amount, globals.BLOCKS_DIR[block].max_stack_size)
+        for slot in inv2.slots:
+            self.assertEqual(slot.type, item)
+            self.assertEqual(slot.amount, globals.ITEMS_DIR[item].max_stack_size)
+
+if __name__ == '__main__':
+    unittest.main()
