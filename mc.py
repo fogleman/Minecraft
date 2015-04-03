f1203eaa013c1e0f5cabe58b5186ff603efa6030
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-04-03 12:08:41 -0600

    Update mc.py

--- mc.py
+++ mc.py
@@ -7,9 +7,14 @@ class World:
   
   def set_block(x, y, z, texture):
     t = (x, y, z)
-    
-    textureToPass = main.GRASS
+    texture.upper()
     if isinstance(texture, str):
-      if texture = "GRASS":
-        
+      if texture = "SAND":
+        textureToPass = main.SAND
+      elif texture = "BRICK":
+        textureToPass = main.BRICK
+      elif texture = "STONE":
+        textureToPass = main.STONE
+      else:
+        textureToPass = main.GRASS
 """
