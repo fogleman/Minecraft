c1659fe0e127052c718477279ab33876c04cf516
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-04-02 18:27:28 -0600

    Update mc.py

--- mc.py
+++ mc.py
@@ -1,3 +1,15 @@
 import main
 
 main.main()
+
+"""
+class World:
+  
+  def set_block(x, y, z, texture):
+    t = (x, y, z)
+    
+    textureToPass = main.GRASS
+    if isinstance(texture, str):
+      if texture = "GRASS":
+        
+"""
