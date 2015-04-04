820fd41dec56f7d527fd074f9b5b85e3f859482e
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-04-03 21:53:41 -0600

    Update mc.py

--- mc.py
+++ mc.py
@@ -9,16 +9,19 @@ class World(Window):
     super()
   
   def set_block(x, y, z, texture):
-    t = (x, y, z)
+    t = x, y, z
     Model.remove_block(t)
     texture.upper()
-    if isinstance(texture, str):
-      if texture = "SAND":
+    if isinstance(texture, (basestring, int, float, long)):
+      if texture = "SAND" or texture == 1:
         textureToPass = main.SAND
-      elif texture = "BRICK":
+      elif texture = "BRICK" or texture == 2:
         textureToPass = main.BRICK
-      elif texture = "STONE":
+      elif texture = "STONE" or texture == 3:
         textureToPass = main.STONE
       else:
         textureToPass = main.GRASS
+    if isinstance(texture, tuple):
+      textureToPass = texture
+    Model.add_block(t, textureToPass)
 """
