586d733d5377b6a8ca9cc958c69f1162f56b9cde
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-04-17 13:07:29 -0600

    Update mc.py

--- mc.py
+++ mc.py
@@ -4,23 +4,41 @@ main.main()
 
 """
 class World(Window):
-
-  def __init__(self):
-    super()
   
-  def set_block(x, y, z, texture):
+  def add_block(x, y, z, texture):
     t = x, y, z
-    texture.upper()
+    textureToPass = texture
     if isinstance(texture, (basestring, int, float, long)):
-      if texture = "SAND" or texture == 1:
+      if isinstance(texture, basestring):
+        texture.upper()
+      if texture == "SAND" or texture == 1:
         textureToPass = main.SAND
-      elif texture = "BRICK" or texture == 2:
+      elif texture == "BRICK" or texture == 2:
         textureToPass = main.BRICK
-      elif texture = "STONE" or texture == 3:
+      elif texture == "STONE" or texture == 3:
         textureToPass = main.STONE
       else:
         textureToPass = main.GRASS
-    if isinstance(texture, tuple):
-      textureToPass = texture
+    #if isinstance(texture, tuple):
+      #textureToPass = texture
     Model.add_block(t, textureToPass)
+  
+  def remove_block(x, y, z):
+    t = x, y, z
+    Modle.remove_block(t)
+  
+  def set_block(x, y, z, texture):
+    add_block(x, y, z, texture)
+  
+  def setFlying(fly):
+    World.flying = fly
+  
+  #def setStrafe(forwardBack, leftRight):
+    #World.strafe = [forwardBack, leftRight]
+  
+  def setPosition(x, y, z):
+    World.position = (x, y, z)
+  
+  def setRotation(xz, ra):
+    World.rotation = (xz, ra)
 """
