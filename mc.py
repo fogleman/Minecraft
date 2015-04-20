a2ae2f27e24010e3e0a1ce0d2f80c7b9e7cbe1de
Rohrbaugh <drohrbaugh9@users.noreply.github.com>
2015-04-20 12:05:07 -0600

    Update mc.py

--- mc.py
+++ mc.py
@@ -2,43 +2,40 @@ import main
 
 class World(main.Window):
 
-  def function_to_make_code_work():
-    a = 0
-
-  #def add_block(x, y, z, texture):
-    #t = x, y, z
-    #textureToPass = texture
-    #if isinstance(texture, (basestring, int, float, long)):
-      #if isinstance(texture, basestring):
-        #texture.upper()
-      #if texture == "SAND" or texture == 1:
-        #textureToPass = main.SAND
-      #elif texture == "BRICK" or texture == 2:
-        #textureToPass = main.BRICK
-      #elif texture == "STONE" or texture == 3:
-        #textureToPass = main.STONE
-      #else:
-        #textureToPass = main.GRASS
-    #Model.add_block(t, textureToPass)
+  def add_block(x, y, z, texture):
+    t = x, y, z
+    textureToPass = texture
+    if isinstance(texture, (basestring, int, float, long)):
+      if isinstance(texture, basestring):
+        texture.upper()
+      if texture == "SAND" or texture == 1:
+        textureToPass = main.SAND
+      elif texture == "BRICK" or texture == 2:
+        textureToPass = main.BRICK
+      elif texture == "STONE" or texture == 3:
+        textureToPass = main.STONE
+      else:
+        textureToPass = main.GRASS
+    Model.add_block(t, textureToPass)
   
-  #def remove_block(x, y, z):
-    #t = x, y, z
-    #Model.remove_block(t)
+  def remove_block(x, y, z):
+    t = x, y, z
+    Model.remove_block(t)
   
-  #def set_block(x, y, z, texture):
-    #add_block(x, y, z, texture)
+  def set_block(x, y, z, texture):
+    add_block(x, y, z, texture)
   
-  #def setFlying(fly):
-    #World.flying = fly
+  def setFlying(fly):
+    World.flying = fly
   
-  ##def setStrafe(forwardBack, leftRight):
-    ##World.strafe = [forwardBack, leftRight]
+  #def setStrafe(forwardBack, leftRight):
+    #World.strafe = [forwardBack, leftRight]
   
-  #def setPosition(x, y, z):
-    #World.position = (x, y, z)
+  def setPosition(x, y, z):
+    World.position = (x, y, z)
   
-  #def setRotation(xz, ra):
-    #World.rotation = (xz, ra)
+  def setRotation(xz, ra):
+    World.rotation = (xz, ra)
 
 def run(w):
   if isinstance(w, (main.Window)):
