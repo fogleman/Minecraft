6c386c632663f286505d28db662c4eab350abfd0
Otavio Carneiro <otavio@carneiro.blog.br>
2015-12-05 17:42:17 -0200

    PEP8

--- main.py
+++ main.py
@@ -12,21 +12,24 @@ SECTOR_SIZE = 16
 if sys.version_info[0] >= 3:
     xrange = range
 
+
 def cube_vertices(x, y, z, n):
     return [
-        x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n, # top
-        x-n,y-n,z-n, x+n,y-n,z-n, x+n,y-n,z+n, x-n,y-n,z+n, # bottom
-        x-n,y-n,z-n, x-n,y-n,z+n, x-n,y+n,z+n, x-n,y+n,z-n, # left
-        x+n,y-n,z+n, x+n,y-n,z-n, x+n,y+n,z-n, x+n,y+n,z+n, # right
-        x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n, # front
-        x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n, # back
+        x-n, y+n, z-n, x-n, y+n, z+n, x+n, y+n, z+n, x+n, y+n, z-n,  # top
+        x-n, y-n, z-n, x+n, y-n, z-n, x+n, y-n, z+n, x-n, y-n, z+n,  # bottom
+        x-n, y-n, z-n, x-n, y-n, z+n, x-n, y+n, z+n, x-n, y+n, z-n,  # left
+        x+n, y-n, z+n, x+n, y-n, z-n, x+n, y+n, z-n, x+n, y+n, z+n,  # right
+        x-n, y-n, z+n, x+n, y-n, z+n, x+n, y+n, z+n, x-n, y+n, z+n,  # front
+        x+n, y-n, z-n, x-n, y-n, z-n, x-n, y+n, z-n, x+n, y+n, z-n,  # back
     ]
+
 
 def tex_coord(x, y, n=4):
     m = 1.0 / n
     dx = x * m
     dy = y * m
     return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
+
 
 def tex_coords(top, bottom, side):
     top = tex_coord(*top)
@@ -44,35 +47,43 @@ BRICK = tex_coords((2, 0), (2, 0), (2, 0))
 STONE = tex_coords((2, 1), (2, 1), (2, 1))
 
 FACES = [
-    ( 0, 1, 0),
-    ( 0,-1, 0),
+    (0, 1, 0),
+    (0, -1, 0),
     (-1, 0, 0),
-    ( 1, 0, 0),
-    ( 0, 0, 1),
-    ( 0, 0,-1),
+    (1, 0, 0),
+    (0, 0, 1),
+    (0, 0, -1),
 ]
 
+
 class TextureGroup(pyglet.graphics.Group):
+
     def __init__(self, path):
         super(TextureGroup, self).__init__()
         self.texture = pyglet.image.load(path).get_texture()
+
     def set_state(self):
         glEnable(self.texture.target)
         glBindTexture(self.texture.target, self.texture.id)
+
     def unset_state(self):
         glDisable(self.texture.target)
+
 
 def normalize(position):
     x, y, z = position
     x, y, z = (int(round(x)), int(round(y)), int(round(z)))
     return (x, y, z)
 
+
 def sectorize(position):
     x, y, z = normalize(position)
     x, y, z = x // SECTOR_SIZE, y // SECTOR_SIZE, z // SECTOR_SIZE
     return (x, 0, z)
 
+
 class Model(object):
+
     def __init__(self):
         self.batch = pyglet.graphics.Batch()
         self.group = TextureGroup('texture.png')
@@ -82,6 +93,7 @@ class Model(object):
         self.sectors = {}
         self.queue = []
         self.initialize()
+
     def initialize(self):
         n = 80
         s = 1
@@ -111,6 +123,7 @@ class Model(object):
                             continue
                         self.init_block((x, y, z), t)
                 s -= d
+
     def hit_test(self, position, vector, max_distance=8):
         m = 8
         x, y, z = position
@@ -123,14 +136,17 @@ class Model(object):
             previous = key
             x, y, z = x + dx / m, y + dy / m, z + dz / m
         return None, None
+
     def exposed(self, position):
         x, y, z = position
         for dx, dy, dz in FACES:
             if (x + dx, y + dy, z + dz) not in self.world:
                 return True
         return False
+
     def init_block(self, position, texture):
         self.add_block(position, texture, False)
+
     def add_block(self, position, texture, sync=True):
         if position in self.world:
             self.remove_block(position, sync)
@@ -140,6 +156,7 @@ class Model(object):
             if self.exposed(position):
                 self.show_block(position)
             self.check_neighbors(position)
+
     def remove_block(self, position, sync=True):
         del self.world[position]
         self.sectors[sectorize(position)].remove(position)
@@ -147,6 +164,7 @@ class Model(object):
             if position in self.shown:
                 self.hide_block(position)
             self.check_neighbors(position)
+
     def check_neighbors(self, position):
         x, y, z = position
         for dx, dy, dz in FACES:
@@ -159,10 +177,12 @@ class Model(object):
             else:
                 if key in self.shown:
                     self.hide_block(key)
+
     def show_blocks(self):
         for position in self.world:
             if position not in self.shown and self.exposed(position):
                 self.show_block(position)
+
     def show_block(self, position, immediate=True):
         texture = self.world[position]
         self.shown[position] = texture
@@ -170,6 +190,7 @@ class Model(object):
             self._show_block(position, texture)
         else:
             self.enqueue(self._show_block, position, texture)
+
     def _show_block(self, position, texture):
         x, y, z = position
         # only show exposed faces
@@ -177,7 +198,7 @@ class Model(object):
         count = 24
         vertex_data = cube_vertices(x, y, z, 0.5)
         texture_data = list(texture)
-        for dx, dy, dz in []:#FACES:
+        for dx, dy, dz in []:  # FACES:
             if (x + dx, y + dy, z + dz) in self.world:
                 count -= 4
                 i = index * 12
@@ -187,31 +208,37 @@ class Model(object):
             else:
                 index += 1
         # create vertex list
-        self._shown[position] = self.batch.add(count, GL_QUADS, self.group, 
-            ('v3f/static', vertex_data),
-            ('t2f/static', texture_data))
+        self._shown[position] = \
+            self.batch.add(count, GL_QUADS, self.group,
+                           ('v3f/static', vertex_data),
+                           ('t2f/static', texture_data))
+
     def hide_block(self, position, immediate=True):
         self.shown.pop(position)
         if immediate:
             self._hide_block(position)
         else:
             self.enqueue(self._hide_block, position)
+
     def _hide_block(self, position):
         self._shown.pop(position).delete()
+
     def show_sector(self, sector):
         for position in self.sectors.get(sector, []):
             if position not in self.shown and self.exposed(position):
                 self.show_block(position, False)
+
     def hide_sector(self, sector):
         for position in self.sectors.get(sector, []):
             if position in self.shown:
                 self.hide_block(position, False)
+
     def change_sectors(self, before, after):
         before_set = set()
         after_set = set()
         pad = 4
         for dx in xrange(-pad, pad + 1):
-            for dy in [0]: # xrange(-pad, pad + 1):
+            for dy in [0]:  # xrange(-pad, pad + 1):
                 for dz in xrange(-pad, pad + 1):
                     if dx ** 2 + dy ** 2 + dz ** 2 > (pad + 1) ** 2:
                         continue
@@ -227,20 +254,26 @@ class Model(object):
             self.show_sector(sector)
         for sector in hide:
             self.hide_sector(sector)
+
     def enqueue(self, func, *args):
         self.queue.append((func, args))
+
     def dequeue(self):
         func, args = self.queue.pop(0)
         func(*args)
+
     def process_queue(self):
         start = time.clock()
         while self.queue and time.clock() - start < 1 / 60.0:
             self.dequeue()
+
     def process_entire_queue(self):
         while self.queue:
             self.dequeue()
 
+
 class Window(pyglet.window.Window):
+
     def __init__(self, *args, **kwargs):
         super(Window, self).__init__(*args, **kwargs)
         self.exclusive = False
@@ -257,13 +290,17 @@ class Window(pyglet.window.Window):
             key._1, key._2, key._3, key._4, key._5,
             key._6, key._7, key._8, key._9, key._0]
         self.model = Model()
-        self.label = pyglet.text.Label('', font_name='Arial', font_size=18, 
-            x=10, y=self.height - 10, anchor_x='left', anchor_y='top', 
-            color=(0, 0, 0, 255))
+        self.label = pyglet.text.Label('', font_name='Arial',
+                                       font_size=18, x=10,
+                                       y=self.height - 10,
+                                       anchor_x='left', anchor_y='top',
+                                       color=(0, 0, 0, 255))
         pyglet.clock.schedule_interval(self.update, 1.0 / 60)
+
     def set_exclusive_mouse(self, exclusive):
         super(Window, self).set_exclusive_mouse(exclusive)
         self.exclusive = exclusive
+
     def get_sight_vector(self):
         x, y = self.rotation
         m = math.cos(math.radians(y))
@@ -271,6 +308,7 @@ class Window(pyglet.window.Window):
         dx = math.cos(math.radians(x - 90)) * m
         dz = math.sin(math.radians(x - 90)) * m
         return (dx, dy, dz)
+
     def get_motion_vector(self):
         if any(self.strafe):
             x, y = self.rotation
@@ -294,6 +332,7 @@ class Window(pyglet.window.Window):
             dx = 0.0
             dz = 0.0
         return (dx, dy, dz)
+
     def update(self, dt):
         self.model.process_queue()
         sector = sectorize(self.position)
@@ -306,6 +345,7 @@ class Window(pyglet.window.Window):
         dt = min(dt, 0.2)
         for _ in xrange(m):
             self._update(dt / m)
+
     def _update(self, dt):
         # walking
         speed = 15 if self.flying else 5
@@ -314,25 +354,27 @@ class Window(pyglet.window.Window):
         dx, dy, dz = dx * d, dy * d, dz * d
         # gravity
         if not self.flying:
-            self.dy -= dt * 0.044 # g force, should be = jump_speed * 0.5 / max_jump_height
-            self.dy = max(self.dy, -0.5) # terminal velocity
+            # g force, should be = jump_speed * 0.5 / max_jump_height
+            self.dy -= dt * 0.044
+            self.dy = max(self.dy, -0.5)  # terminal velocity
             dy += self.dy
         # collisions
         x, y, z = self.position
         x, y, z = self.collide((x + dx, y + dy, z + dz), 2)
         self.position = (x, y, z)
+
     def collide(self, position, height):
         pad = 0.25
         p = list(position)
         np = normalize(position)
-        for face in FACES: # check all surrounding blocks
-            for i in xrange(3): # check each dimension independently
+        for face in FACES:  # check all surrounding blocks
+            for i in xrange(3):  # check each dimension independently
                 if not face[i]:
                     continue
                 d = (p[i] - np[i]) * face[i]
                 if d < pad:
                     continue
-                for dy in xrange(height): # check each height
+                for dy in xrange(height):  # check each height
                     op = list(np)
                     op[1] -= dy
                     op[i] += face[i]
@@ -344,12 +386,14 @@ class Window(pyglet.window.Window):
                         self.dy = 0
                     break
         return tuple(p)
+
     def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
         return
         x, y, z = self.position
         dx, dy, dz = self.get_sight_vector()
         d = scroll_y * 10
         self.position = (x + dx * d, y + dy * d, z + dz * d)
+
     def on_mouse_press(self, x, y, button, modifiers):
         if self.exclusive:
             vector = self.get_sight_vector()
@@ -364,6 +408,7 @@ class Window(pyglet.window.Window):
                     self.model.add_block(previous, self.block)
         else:
             self.set_exclusive_mouse(True)
+
     def on_mouse_motion(self, x, y, dx, dy):
         if self.exclusive:
             m = 0.15
@@ -371,6 +416,7 @@ class Window(pyglet.window.Window):
             x, y = x + dx * m, y + dy * m
             y = max(-90, min(90, y))
             self.rotation = (x, y)
+
     def on_key_press(self, symbol, modifiers):
         if symbol == key.W:
             self.strafe[0] -= 1
@@ -382,7 +428,7 @@ class Window(pyglet.window.Window):
             self.strafe[1] += 1
         elif symbol == key.SPACE:
             if self.dy == 0:
-                self.dy = 0.015 # jump speed
+                self.dy = 0.015  # jump speed
         elif symbol == key.ESCAPE:
             self.set_exclusive_mouse(False)
         elif symbol == key.TAB:
@@ -390,6 +436,7 @@ class Window(pyglet.window.Window):
         elif symbol in self.num_keys:
             index = (symbol - self.num_keys[0]) % len(self.inventory)
             self.block = self.inventory[index]
+
     def on_key_release(self, symbol, modifiers):
         if symbol == key.W:
             self.strafe[0] += 1
@@ -399,6 +446,7 @@ class Window(pyglet.window.Window):
             self.strafe[1] += 1
         elif symbol == key.D:
             self.strafe[1] -= 1
+
     def on_resize(self, width, height):
         # label
         self.label.y = height - 10
@@ -407,9 +455,16 @@ class Window(pyglet.window.Window):
             self.reticle.delete()
         x, y = self.width // 2, self.height // 2
         n = 10
-        self.reticle = pyglet.graphics.vertex_list(4,
-            ('v2i', (x - n, y, x + n, y, x, y - n, x, y + n))
-        )
+        self.reticle = \
+            pyglet.graphics.vertex_list(4,
+                                        ('v2i',
+                                         (x - n,
+                                          y, x + n,
+                                          y, x, y - n,
+                                          x, y + n)
+                                         )
+                                        )
+
     def set_2d(self):
         width, height = self.get_size()
         glDisable(GL_DEPTH_TEST)
@@ -419,6 +474,7 @@ class Window(pyglet.window.Window):
         glOrtho(0, width, 0, height, -1, 1)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
+
     def set_3d(self):
         width, height = self.get_size()
         glEnable(GL_DEPTH_TEST)
@@ -433,6 +489,7 @@ class Window(pyglet.window.Window):
         glRotatef(-y, math.cos(math.radians(x)), 0, math.sin(math.radians(x)))
         x, y, z = self.position
         glTranslatef(-x, -y, -z)
+
     def on_draw(self):
         self.clear()
         self.set_3d()
@@ -442,6 +499,7 @@ class Window(pyglet.window.Window):
         self.set_2d()
         self.draw_label()
         self.draw_reticle()
+
     def draw_focused_block(self):
         vector = self.get_sight_vector()
         block = self.model.hit_test(self.position, vector)[0]
@@ -452,15 +510,18 @@ class Window(pyglet.window.Window):
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
             pyglet.graphics.draw(24, GL_QUADS, ('v3f/static', vertex_data))
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+
     def draw_label(self):
         x, y, z = self.position
         self.label.text = '%02d (%.2f, %.2f, %.2f) %d / %d' % (
-            pyglet.clock.get_fps(), x, y, z, 
+            pyglet.clock.get_fps(), x, y, z,
             len(self.model._shown), len(self.model.world))
         self.label.draw()
+
     def draw_reticle(self):
         glColor3d(0, 0, 0)
         self.reticle.draw(GL_LINES)
+
 
 def setup_fog():
     glEnable(GL_FOG)
@@ -471,12 +532,14 @@ def setup_fog():
     glFogf(GL_FOG_START, 20.0)
     glFogf(GL_FOG_END, 60.0)
 
+
 def setup():
     glClearColor(0.5, 0.69, 1.0, 1)
     glEnable(GL_CULL_FACE)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
     setup_fog()
+
 
 def main():
     window = Window(width=800, height=600, caption='Pyglet', resizable=True)
