33c679b81029c397136762baa527cb648a1af533
Otavio Carneiro <otaviocsantos@gmail.com>
2015-12-06 13:56:13 -0200

    Cleaning up

--- main.py
+++ main.py
@@ -25,23 +25,6 @@ def cube_vertices(x, y, z, n):
     ]
 
 
-def tex_coord(x, y, n=4):
-    m = 1.0 / n
-    dx = x * m
-    dy = y * m
-    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
-
-
-def tex_coords(top, bottom, side):
-    top = tex_coord(*top)
-    bottom = tex_coord(*bottom)
-    side = tex_coord(*side)
-    result = []
-    result.extend(top)
-    result.extend(bottom)
-    result.extend(side * 4)
-    return result
-
 GRASS = block.GRASS.texture  # tex_coords((1, 0), (0, 1), (0, 0))
 SAND = block.SAND.texture  # tex_coords((1, 1), (1, 1), (1, 1))
 BRICK = block.BRICK.texture  # tex_coords((2, 0), (2, 0), (2, 0))
