cb6e80ecca60d7b724263d4e832e2ef9716562d7
Otavio Carneiro <otaviocsantos@gmail.com>
2015-12-06 12:34:29 -0200

    First unittests

---
+++ block.py
@@ -0,0 +1,3 @@
+
+class Block():
+    pass
---
+++ test_blocks.py
@@ -0,0 +1,8 @@
+import unittest
+
+from block import Block
+
+class BlockTest(unittest.TestCase):
+
+    def test_initialize_block(self):
+        block = Block()
