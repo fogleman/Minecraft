8ef7e05af78ba4815d9fd2a59031061d818c35da
Otavio Carneiro <otavio@carneiro.blog.br>
2015-12-21 13:02:23 -0200

    Prototype for text interface

---
+++ txtui/prototype.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+""" Prototype text interface with random map """
+
+import random
+
+for y in range(20):
+    line = ''
+    for x in range(40):
+        line = line + random.choice([' ', '#'])
+    print(line)
+    
